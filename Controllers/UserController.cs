e0c0acf0c566dbd7d4f4260e7e322378f51cbc7c
chase <cverbout@gmail.com>
2022-08-05 16:00:22 -0700

    Login Request & Delete User method. Pausing Backend- working on Front end to figure out backend shortcomings.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -45,12 +45,54 @@ namespace SnaggleAPI.Controllers
             return Ok("User created!");
         }
 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginRequest request)
+        {
+           var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.UserName);
+           if (user == null)
+           {
+                return BadRequest("NOpe");
+           }
+
+            if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Password wrong");
+            }
+
+            return Ok($"Welcome back, {user.Username}! :)");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var user = await _context.Users.FindAsync(Id);
+
+            if (user == null)
+            {
+                return BadRequest("Doesnt Exist");
+            }
+            
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok("Deleted!");
+        }
+
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using (var hmac = new HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
---
+++ Models/UserLoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnaggleAPI.Models
+{
+    public class UserLoginRequest
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
